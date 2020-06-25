27c9dc604d54764153f6fa8a3535f824584493d4
akhil-prasad94 <42665938+akhil-prasad94@users.noreply.github.com>
2020-06-25 17:08:47 -0400

    Adding static files

---
+++ src/main/resources/static/precache-manifest.b1d1666f9c78e663d886772e3d3de803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddeeeea73bd1f1dcb367c9c61e4ec11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2462278c6690a39c5d54",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "25549867a50d0871be8c",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "2462278c6690a39c5d54",
+    "url": "/static/js/2.0308b9c3.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.0308b9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25549867a50d0871be8c",
+    "url": "/static/js/main.53123982.chunk.js"
+  },
+  {
+    "revision": "c081e2f36df4981bab2b",
+    "url": "/static/js/runtime-main.8b4b340c.js"
+  }
+]);
